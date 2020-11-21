5b76d6d4772e09598de6029e6e2f3b2e53db4955
rkumar061 <rkumarkumawat016@gmail.com>
2020-11-21 05:47:33 +0530

    upload2
    fdsgerg

--- db/temp/errors.php
+++
@@ -1,8 +0,0 @@
-<?php 
-		if(isset($_SESSION['error'])) {
-			foreach ($_SESSION['error'] as  $value) {
-				echo "<font color='red'>".$value."</font><br>";
-			}
-			unset($_SESSION['error']);
-		}
-	?>
